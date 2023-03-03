eded4d7dfb413e24575527a97effaf2f86d35a5a
Pranjal Singh <pranjalsingh9304@gmail.com>
2023-03-04 00:43:37 +0530

    exit

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("\033[1;34m"); // set text color to blue
+    printf("\033[48;5;11m"); // set background color to light blue
+    printf("************************************************************\n");
+    printf("*                                                          *\n");
+    printf("*                WELCOME TO MY REAL ESTATE ADVISOR          *\n");
+    printf("*                                                          *\n");
+    printf("************************************************************\n");
+    printf("\033[0m"); // reset text and background color
+    printf("\n[1] Add New Record\n");
+    printf("[2] Property Listing\n");
+    printf("[3] Search\n");
+    printf("[4] Buy/sell\n");
+    printf("[5] Delete Record\n");
+    printf("[0] Exit\n");
+    printf("Select Any Option from above: ");
+    // scanf("%d",&a)
+    return 0;
+}
--- src/realstaye.c
+++ src/realstaye.c
@@ -3,7 +3,6 @@
 int main(){
     int a ,b ,c;
     printf("Choose any Number");
-    printf
     scanf("%d",&a);
     printf("");
 }
